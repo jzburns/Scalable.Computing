375a5615359da74ec0844b81148bb9736449e2a6
John Burns <jzburns@gmail.com>
2014-11-24 20:08:00 +0000

    modified the file

--- week11/openMP2.c
+++ week11/openMP2.c
@@ -7,9 +7,16 @@ int main (void)
     int t = 9;
     int r = 5;
 
+    #pragma omp parallel 
+    if (omp_get_thread_num() == 0 ) {
+      int nthreads = omp_get_num_threads();
+      printf("There are %d threads\n",nthreads);
+    }
+
     #pragma omp parallel for 
     for(int j = 0; j < 10; j++) {
-      printf("thread %d: i = %d\n", omp_get_thread_num(), j);
+      int tid = omp_get_thread_num();
+      printf("thread %d: i = %d\n", tid, j);
     }
 
     #pragma omp parallel firstprivate(q,t,r) 
