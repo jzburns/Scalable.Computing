7cf41bdb50c12a8aef39a79c3d7a747778a0a5de
John Burns <jzburns@gmail.com>
2016-10-17 16:41:10 +0100

    openmp

---
+++ week5/week5.C
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <omp.h>
+
+const int SZ = 1000000;
+
+double calcAvg(int*);
+
+int main (int argc, char** argv) {
+	srand(time(NULL));
+
+	int vals[SZ];
+	int val1 = 67;
+	int val2 = 99;
+
+	for(int i = 0; i < SZ; i++){
+		int r = rand() % 100;	
+		vals[i] = r;
+	}
+
+	int cntr = 0;
+	for(int i = 0; i < SZ; i++){
+		if(vals[i] >= val1 && vals[i] <= val2) {
+			cntr++;
+		}
+	}
+
+	printf("ST result: %d\n", cntr);	
+
+	cntr = 0;
+
+	#pragma omp parallel 
+	{
+		if(!omp_get_thread_num()) {
+			int num_thr = omp_get_num_threads();
+			printf("MT threads: %d\n", num_thr);  
+		}
+	}
+
+	#pragma omp parallel for reduction (+:cntr)
+	for(int i = 0; i < SZ; i++){
+		if(vals[i] >= val1 && vals[i] <= val2) {
+			cntr++;
+		}
+	}
+
+	printf("MT result: %d\n", cntr);	
+
+	
+}
+
