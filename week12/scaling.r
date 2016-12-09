1e5197381e3c84035589322a55a7078dbed2ce1e
jburns <jzburns@gmail.com>
2016-12-09 18:11:49 +0000

    modified

--- week12/scaling.r
+++ week12/scaling.r
@@ -1,19 +1,24 @@
-computeEff <- function(n) {
-	p <- c(1:32)
-
+computeEff <- function(p) {
+	n <- 2^(6:20)
 	Ts <- n
-	# change this to reflect the 
-	# parallel complexity
-	Tp <- n/p + 2*p
+	Tp <-  n/p + (2 * log2(p))
 	To <- p*Tp - Ts
-	
-	E <- 1 / (1 + To / Ts)
+	E <- round(1 / (1 + To / Ts), digits=2)
 }
 
-plot(computeEff(64), type='b', pch=18, xlab="p", ylab="E")
-legend("topright", inset=.05, title="input size n",
-   c("64","192","320", "512"), pch=18:21)
-lines(computeEff(192), type='b', pch=19)
-lines(computeEff(320), type='b', pch=20)
-lines(computeEff(512), type='b', pch=21)
-lines(computeEff(2048), type='b', pch=22)
+computeOhead <- function(n) {
+	p <- c(1:32)
+	Ts <- n
+	Tp <- n/p + 2*p
+	To <- p*Tp - Ts
+}
+
+computeS <- function(p, n) {
+	Ts <- n
+	Tp <- n/p + 2*p
+	S <- Ts / Tp
+}
+
+options(width=150)
+p <- 2^(0:14)
+print(sapply(p, FUN=computeEff))
