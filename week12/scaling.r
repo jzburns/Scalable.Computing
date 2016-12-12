610a75ba07f4b8fc1732f9ec6d2f0b149d801a9a
jburns <jzburns@gmail.com>
2016-12-12 13:13:53 +0000

    mods

--- week12/scaling.r
+++ week12/scaling.r
@@ -1,20 +1,33 @@
 # scaling equation of efficiency
 
-p <- 2^(0:12)
+p <- 2^(0:14)
 n <- 2^(6:20)
 
 computeEff <- function(p) {
 	Ts <- n
-	Tp <-  n/p + (2 * log2(p))
+	Tp <-  n/p + (2 * log2(n))
 	To <- p*Tp - Ts
 	E <- round(1 / (1 + To / Ts), digits=2)
 }
 
-computeOhead <- function(n) {
-	p <- c(1:32)
+computeEff2 <- function(p) {
+        Ts <- n^3
+        Tp <-  n^3/p + (5 * p * n)
+        To <- p*Tp - Ts
+        E <- round(1 / (1 + To / Ts), digits=2)
+}
+
+computeOhead <- function(p) {
 	Ts <- n
-	Tp <- n/p + 2*p
+	Tp <-  n/p + (2 * log2(n))
+	To <- round(p*Tp - Ts, digits=2)
 	To <- p*Tp - Ts
+}
+
+computeOhead2 <- function(n) {
+        Ts <- n^3
+        Tp <-  n^3/p + (5 * p * n)
+        To <- round(p*Tp - Ts, digits=2)
 }
 
 computeS <- function(p, n) {
@@ -23,8 +36,32 @@ computeS <- function(p, n) {
 	S <- Ts / Tp
 }
 
+#### plotting the efficiency
 m <- sapply(p, FUN=computeEff)
 rownames(m) <- n
 colnames(m) <- p
-print(m)
-matplot(rownames(m), m, type="b", log="x")
+matplot(main="Linear", rownames(m), m, type="b", log="x")
+
+x11()
+m <- sapply(p, FUN=computeEff2)
+rownames(m) <- n
+colnames(m) <- p
+matplot(main="Cubic", rownames(m), m, xlab="n", type="b", log="x")
+
+#### plotting the overhead
+x11()
+m <- sapply(p, FUN=computeOhead)
+rownames(m) <- n
+colnames(m) <- p
+# print(m)
+matplot(main="Linear", rownames(m), m, xlab="n", type="b", log="xy")
+lines (n, n)
+
+x11()
+m <- sapply(p, FUN=computeOhead2)
+rownames(m) <- n
+colnames(m) <- p
+matplot(main="Cubic", rownames(m), m, xlab="n", type="b", log="xy")
+lines (n, n^3)
+
+
